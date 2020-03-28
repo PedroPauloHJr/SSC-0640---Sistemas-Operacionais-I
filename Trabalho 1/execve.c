6c7c807800a05677eb7fc46f3a1557e634db15e0
Georgegantus <40300007+Georgegantus@users.noreply.github.com>
2020-03-28 16:10:39 -0300

    Add files via upload

---
+++ Trabalho 1/execve.c
@@ -0,0 +1,18 @@
+// O consagrado                                       nUSP:
+// George Alexandre Gantus                            nUSP: 10691988
+// Pedro Paulo Herzog Junior                          nUSP: 10284692
+
+
+//Neste programa vamos usar a chamada de sistema execve da categoria gerenciamento de processos
+//Execve- A chamda de sistema execve cria um processo ao executar um programa neste caso vamos executar o programa que usa as outras system calls de processos
+//execve(caminho do arquivo,argumentos,ambiente)
+#include <stdio.h>
+#include <unistd.h>
+int main(void) {
+  printf("Programa que chama a main\n");//mostra que este processo é iniciado
+  char* argv[] = { "ola", "mundo", NULL };//declara os argumentos usados na abertura do programa
+  char* envp[] = { "", "environment", NULL };//o ambiente que vamos utilizar
+  execve("./main", argv, envp);//cria o novo processo com o programa
+  perror("Erro ao usar o execve");//nao sera executado caso o execve ocorra com sucesso
+  return 0;
+}
