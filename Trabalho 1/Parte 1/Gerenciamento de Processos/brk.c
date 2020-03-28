43faca626422c80a95f2aac1107933e4e77e7917
Pedro Paulo Herzog Junior <pedropaulohjr@usp.br>
2020-03-28 16:10:09 -0300

    Delete brk.c

--- Trabalho 1/Parte 1/Gerenciamento de Processos/brk.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(void) {
-  void *a = sbrk(0);//pega o final atual do programa
-  int *p = (int *)a;//faz a conversao do valor do ponteiro para inteiro
-  brk(p+10);//seta o novo final para o final +10 bytes
-  printf("memoria é %d",*p);
-}
