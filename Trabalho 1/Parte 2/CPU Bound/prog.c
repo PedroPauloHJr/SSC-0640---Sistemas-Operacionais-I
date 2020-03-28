45491020f40459c6198d080f46ce901cbd53b474
Pedro Paulo Herzog Junior <pedropaulohjr@usp.br>
2020-03-28 19:21:05 -0300

    Add files via upload

---
+++ Trabalho 1/Parte 2/CPU Bound/prog.c
@@ -0,0 +1,27 @@
+// George Alexandre Gantus                            nUSP: 10691988
+// Pedro Paulo Herzog Junior                          nUSP: 10284692
+// Vinícius Molina Garcia                             nUSP:  8929296
+
+// Neste programa, calcularemos o valor do INT_MAX.
+
+// Bibliotecas utilizadas:
+#include <stdio.h>
+#include <limits.h>
+
+// Iniciando o programa:
+int main(void)
+{
+  // Declarando as variaveis necssarias para a execucao do algoritmo:
+  int i;
+
+  // Calculando o valor de INT_MAX:
+  for(i = 0; i < INT_MAX; i++)
+  {}
+
+  // Imprimindo os valores obtidos:
+  printf("O valor de i é: %d\n", i);
+  printf("O valor do INT MAX é %d\n", INT_MAX);
+
+  // Encerra o programa:
+  return 0;
+}
