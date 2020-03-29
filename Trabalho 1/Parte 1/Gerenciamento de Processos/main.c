908a145b19852a552b3f36fd208913ae3bc15e5d
Pedro Paulo Herzog Junior <pedropaulohjr@usp.br>
2020-03-29 17:14:54 -0300

    Update main.c

--- Trabalho 1/Parte 1/Gerenciamento de Processos/main.c
+++ Trabalho 1/Parte 1/Gerenciamento de Processos/main.c
@@ -1,6 +1,6 @@
-// O consagrado                                       nUSP:
 // George Alexandre Gantus                            nUSP: 10691988
 // Pedro Paulo Herzog Junior                          nUSP: 10284692
+// Vinícius Molina Garcia                             nUSP:  8929296
 
 //Neste codigo faremos o uso das system calls getpid,getgid e fork todas relacionadas a gerenciamento de processos
 //getpid retorna o id do processo - getpid(void)
